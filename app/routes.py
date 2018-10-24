f5878f2052b0850c44a994167e8da8dab6ad13da
Carl Rumsey <rumseyc@mail.gvsu.edu>
2018-10-24 04:11:04 -0400

    Use for loop for looping through posts in html

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,19 @@
 from flask import render_template
 from app import app
 
+
 @app.route('/')
 @app.route('/index')
-@app.route('/test')
-
 def index():
     user = {'username': 'Carl Rumsey'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'Jordi'},
+            'body': 'Euchre is too easy!'
+        },
+        {
+            'author': {'username': 'Pepe'},
+            'body': 'Im never playing with you again!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
